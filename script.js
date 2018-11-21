75231a9228edb03939bae415c89a5274f6ac0521
Ghassan Taha <ghassan.m.taha@gmail.com>
2018-11-21 00:33:51 -0700

    styling and sound

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ var buttons = document.querySelectorAll('.button, .buttons');
 
 buttons.forEach(element => { element.addEventListener("click", control)});
 
-var running = false;
-
+var session = "work";
+var timer;
+var state = 0; //0:stopped, 1:running, 2: paused
+var audio = document.querySelector('audio');
 function control(e){
 	
 	switch(e.target.id){
 
 		case "play":
-			break;
 		case "pause":
-			break;
 		case "reset":
+			runTimer(e.target.id);
 			break;
 		case "workplus":
 		case "workminus":
@@ -23,12 +24,99 @@ function control(e){
 		default:
 			break;
 	}
+}
+function runTimer(button){
+	var display = document.querySelector("#counter");
+	var interval = document.querySelector("#work");
+	var sessionDisplay = document.querySelector("#stateLabel");
+
+	if (state == 0 ){
+		// stopped
+		if (button == "play"){
+			
+			state = 1;
+			display.textContent = interval.textContent;
+			sessionDisplay.textContent = "Work";
+			timer = window.setInterval(displayUpdate, 1000);
+
+		}
+	} else if (state == 1){
+		// running
+		if (button == "reset") {
+			
+			window.clearInterval(timer);
+			state = 0;
+			display.textContent = "00:00";
+			sessionDisplay.textContent = "";
+			
+		} else if (button == "pause"){
+
+			pauseValue = sessionDisplay.textContent;
+			state = 2;
+			window.clearInterval(timer);
+			sessionDisplay.textContent = "Paused";
+			
+		}
+	} else { 
+		//state == 2
+		//paused
+		if (button == "reset") {
+			
+			state = 0;
+			display.textContent = "00:00";
+			sessionDisplay.textContent = "";
+			pauseValue = "";
+
+		} else if (button == "play"){
+
+			state = 1; 
+			timer = window.setInterval(displayUpdate, 1000);
+			sessionDisplay.textContent = pauseValue;
+		}
+
+	}
+
+}
+
+function displayUpdate() {
+	var display = document.querySelector("#counter");
+	var sessionDisplay = document.querySelector("#stateLabel");
+
+	var secondsValue = display.textContent.slice(display.textContent.indexOf(":")+1);
+	var minutesValue = display.textContent.slice(0, display.textContent.indexOf(":"));
+	
+	var seconds = parseInt(secondsValue) + 60*parseInt(minutesValue);
+
+	if ( seconds == 0 ){
+		//switch
+		var interval;
+		var audio;
+		if(session == "work"){
+			interval = document.querySelector("#break");
+			session = "break";
+			sessionDisplay.textContent = "Rest"
+		} else {
+			interval = document.querySelector("#work")
+			session = "work";
+			sessionDisplay.textContent = "Work"
+		}
+		display.textContent = interval.textContent;
+		
+		audio.play();
+
+	} else {
+		seconds--;
+		minutesValue = (seconds/60 > 9 ? ""+Math.floor(seconds/60) : "0"+Math.floor(seconds/60));
+		secondsValue = (Math.floor(seconds%60) > 9 ? ":"+Math.floor(seconds%60) :":0"+Math.floor(seconds%60));
+		display.textContent = minutesValue + secondsValue;
+	}
 
 }
 
 function sessionTimes(button){
-	
+		
 	var element;
+	
 	if(button == "workplus" || button == "workminus" ){
 		element = "work";
 	} else {
@@ -44,7 +132,9 @@ function sessionTimes(button){
 	if (button == "workplus" || button == "breakplus"){
 		minutes++;
 	} else {
-		minutes--;
+		if (minutes > 0){
+			minutes--;
+		}
 	}
 
 	if(minutes < 10){ 
@@ -57,8 +147,6 @@ function sessionTimes(button){
 
 	setTime.textContent = text;
 
-	msg(minutes);
-	msg(text);
 }
 
 function msg(content)
